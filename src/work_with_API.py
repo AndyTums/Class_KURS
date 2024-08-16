3d653ce01bab4cb8cb448bd5278b860178cf1eb1
Andrey Tumerkin <tumerrkin@gmail.com>
2024-08-16 19:39:10 +0300

    add new class Vacancies + method

--- src/work_with_API.py
+++ src/work_with_API.py
@@ -4,22 +4,23 @@ from src.abstract_classes import GetAPI
 
 
 class HeadHunter(GetAPI):
-    """ Клас наследуется от абстрактного класса для работы с площадкой HeadHunter. """
+    """ Клас наследуется от абстрактного класса для работы с площадкой HeadHunter.
+        Включает в себя: получение информации всех вакансий, а по наименованию """
 
     def __init__(self):
         self.__url = "https://api.hh.ru/vacancies"
         self.__params = {'text': "", "page": 0, "per_page": 100}
         self.__code_status = requests.get(self.__url).status_code
-        self.__vacancies = []
 
     def get_info(self):
         """ Метод для подключения к API и получение данных """
+
         if self.__code_status != 200:
-            raise NameError(f"Не возможно получить данные, ошибка: Код {self.code_status}")
+            raise NameError(f"Не возможно получить данные, ошибка: Код {self.__code_status}")
 
         else:
             response = requests.get(self.__url, self.__params)
-            return response.json()["items"]
+            return response.json()["items"][0]
 
     def get_vacancies(self, vacancies: str = None) -> list:
         """ Метод получающий информацию по заданной вакансии, в противном случае выводит все вакансии """
@@ -36,12 +37,9 @@ class HeadHunter(GetAPI):
                 return HeadHunter.get_info(self)
 
 
-# if __name__ == "__main__":
-#
-#     head = HeadHunter()
-#     # print(head.get_info())
-#     print(head.get_vacancies("курьер"))
-#
-#     # url = "https://api.hh.ru/vacancies"
-#     # response = requests.get(url)
-#     # print(response.json()["items"])
+if __name__ == "__main__":
+
+    head = HeadHunter()
+    print(head.get_info())
+    # print(head.get_vacancies("курьер"))
+
